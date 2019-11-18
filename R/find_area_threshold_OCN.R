8fbeed56bbb2fae2b201fbd680155df7273b06fa
Carraro <luca.carraro@eawag.ch>
2019-11-18 08:47:48 +0100

    Fix find_area_threshold_OCN

--- R/find_area_threshold_OCN.R
+++ R/find_area_threshold_OCN.R
@@ -1,6 +1,6 @@
 
 find_area_threshold_OCN <- function(OCN,
-                                    thr_values=seq(OCN$cellsize,OCN$cellsize*max(OCN$FD$A),OCN$cellsize),
+                                    thr_values=seq(OCN$cellsize,max(OCN$FD$A),OCN$cellsize),
                                     MaxReachLength=Inf,
                                     StreamOrderType="Strahler",
                                     DisplayUpdates=0){
