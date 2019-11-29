dab436c6fb7845618fc6c2331b51fc9bd9852bae
Carraro <luca.carraro@eawag.ch>
2019-11-29 15:21:15 +0100

    Update documentation

--- R/draw_contour_OCN.R
+++ R/draw_contour_OCN.R
@@ -2,7 +2,12 @@
 draw_contour_OCN <- function(OCN,
                              A_thr_draw=0.002*OCN$dimX*OCN$dimY*OCN$cellsize^2,
                              ExactDraw=TRUE,
-                             DrawOutlets=0){
+                             ColPal_river=NULL,
+                             ColPal_cont="#000000",
+                             DrawOutlets=0,
+                             pch=22,
+                             col="#000000",
+                             bg="#000000"){
   
   if (!("X_draw" %in% names(OCN$FD))){
     stop('Missing fields in OCN. You should run landscape_OCN prior to draw_contour_OCN.')
@@ -26,17 +31,38 @@ draw_contour_OCN <- function(OCN,
   }
   
   # plot network (draw mode)
-  if (OCN$N_outlet > 1){
-    rnbw <- hcl.colors(OCN$N_outlet,palette="Dark 3")
-    rnbw <- c(rnbw[(round(OCN$N_outlet/2)+1):OCN$N_outlet],rnbw[1:(round(OCN$N_outlet/2)+1)])
-  } else {rnbw <- hcl.colors(3,palette="Dark 3")
-  rnbw <- rnbw[3]}
+  
+  if (is.null(ColPal_river)){
+    if (OCN$N_outlet > 1){
+      rnbw <- hcl.colors(OCN$N_outlet,palette="Dark 3")
+      rnbw <- c(rnbw[(round(OCN$N_outlet/2)+1):OCN$N_outlet],rnbw[1:(round(OCN$N_outlet/2)+1)])
+    } else {rnbw <- hcl.colors(3,palette="Dark 3")
+    rnbw <- rnbw[3]}
+    ColPal_river <- rnbw
+    
+  } else if (typeof(ColPal_river)=="closure") {
+    ColPal_river <- ColPal_river(OCN$N_outlet)
+  } else if (typeof(ColPal_river)=="character") {
+    if (length(ColPal_river)==1){
+      ColPal_river <- rep(ColPal_river,OCN$N_outlet)
+    }
+  }
+  
+  if (ColPal_cont==0){
+    ColPal_cont=ColPal_river
+  } else if (typeof(ColPal_cont)=="closure") {
+    ColPal_cont <- ColPal_cont(OCN$N_outlet)
+  } else if (typeof(ColPal_cont)=="character") {
+    if (length(ColPal_cont)==1){
+      ColPal_cont <- rep(ColPal_cont,OCN$N_outlet)
+    }
+  }
   
   AvailableNodes <- setdiff(1:OCN$FD$Nnodes,OCN$FD$Outlet)
   par(bty="n",mar=c(1,1,1,1))
   plot(c(min(X_draw),max(X_draw)),c(min(Y_draw),max(Y_draw)),type="n",xlab=" ",ylab=" ",axes=FALSE,asp=1)
   
-  if (DrawOutlets==1) {points(X_draw[OCN$FD$Outlet],Y_draw[OCN$FD$Outlet],pch=22,col="#000000",bg="#000000")}
+  if (DrawOutlets==1) {points(X_draw[OCN$FD$Outlet],Y_draw[OCN$FD$Outlet],pch=pch,col=col,bg=bg)}
   
   if (EasyDraw==FALSE){
     for (i in AvailableNodes){
@@ -49,14 +75,14 @@ draw_contour_OCN <- function(OCN,
         abs(X_draw[i]-X_draw[OCN$FD$DownNode[i]]) <= OCN$cellsize & 
         abs(Y_draw[i]-Y_draw[OCN$FD$DownNode[i]]) <= OCN$cellsize  ) {
       lines(c(X_draw[i],X_draw[OCN$FD$DownNode[i]]),c(Y_draw[i],Y_draw[OCN$FD$DownNode[i]]),
-            lwd=0.5+4.5*(OCN$FD$A[i]/(OCN$FD$Nnodes*OCN$cellsize^2))^0.5,col=rnbw[OCN$FD$to_CM[i]])}
+            lwd=0.5+4.5*(OCN$FD$A[i]/(OCN$FD$Nnodes*OCN$cellsize^2))^0.5,col=ColPal_river[OCN$FD$to_CM[i]])}
   }
   for (j in 1:OCN$N_outlet){
     for (k in 1:length(Xc[[j]])){
-      lines(Xc[[j]][[k]],Yc[[j]][[k]],lwd=2)
+      lines(Xc[[j]][[k]],Yc[[j]][[k]],lwd=2,col=ColPal_cont[j])
     }
   }
   
-  if (DrawOutlets==2) {points(X_draw[OCN$FD$Outlet],Y_draw[OCN$FD$Outlet],pch=22,col="#000000",bg="#000000")}
+  if (DrawOutlets==2) {points(X_draw[OCN$FD$Outlet],Y_draw[OCN$FD$Outlet],pch=pch,col=col,bg=bg)}
   
 }
